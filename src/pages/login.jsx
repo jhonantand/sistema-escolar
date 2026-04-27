c44c93a91c1ad3162f7aae05ddad37b67ab95d1c
Jhonantan Dias <jhonantandias36@gmail.co>
2026-04-26 22:56:28 -0300

    corrigindo login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "../supabase";
+import { supabase } from "../lib/supabase";
 import "./login.css";
 
 export default function Login({ onLogin }) {
