eb1b16b112e89f64e6439b454baae48ea4d1025e
Jhonantan Dias <jhonantandias36@gmail.co>
2026-04-26 23:52:57 -0300

    corrigindo login

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://bshsnbvopzzaeqerwfww.supabase.co";
+const supabaseAnonKey = "sb_publishable_YAAkV8BIC0ObzFIihgWeEQ_99ToLTWc";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
