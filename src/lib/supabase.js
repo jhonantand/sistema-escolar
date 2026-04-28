eb8b1ca9fae2c9702a3c9879cf82a4fcec383049
Jhonantan Dias <jhonantandias36@gmail.co>
2026-04-27 22:10:41 -0300

    melhorando visual do login

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://bshsnbvopzzaeqerwfww.supabase.co";
-const supabaseAnonKey = "sb_publishable_YAAkV8BIC0ObzFIihgWeEQ_99ToLTWc";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
